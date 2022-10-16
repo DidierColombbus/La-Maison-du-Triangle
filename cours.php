417ba136ea214f773bf894c463e3fc370d34674b
DidierColombbus <100928062+DidierColombbus@users.noreply.github.com>
2022-10-16 22:31:33 +0200

    Update cours.php

--- cours.php
+++ cours.php
@@ -6,12 +6,42 @@
 require_once("inc/init.inc.php");
 
 // Cours affichera les cours que nous proposerons à nos clients.
-// Il y aura une description complète des cours, avec les âges proposés, un bouton de réservation, le nombre maximal de participants. 
-$content .= '<div  class="bg-light container py-1">';
+// Il y aura une description complète des cours, avec les âges proposés, un bouton de réservation, le nombre maximal de participants.
+
+$requete = $pdo->query('SELECT * FROM cours');
+$cours = $requete->fetchAll(PDO::FETCH_ASSOC);
+$nbcours = $requete->rowCount();
+if($nbcours != 0){
+
+$content .= '<h1 class="bg-light"> ლ(╹◡╹ლ) Super, c\'est l\'heure des cours !</h1><br>';
+$content.= '<div class="card border-dark w-75 text-start">';
+
+    $content.= '<div class="row g-0">';
+
+    $content.= '<div class="col-sm-12">';
+    $content.= '<div class="card-body">';
+    $content.= '<h4 class="card-title">' . $cours['titre_cours'] . '</h4>';
+    $content.= '<p class="card-text">' . $cours['description_cours'] . '</p>';
+    $content.= '  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Date : ' . $cours['date'] . '</li>
+    <li class="list-group-item">Prix : ' . $cours['prix_cours'] . '</li>
+  </ul>';
+$content.= '  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Places : ' . $cours['nb_places'] . '</li>
+    <li class="list-group-item">Réservées : ' . $cours['nb_participants'] . '</li>
+  </ul>';
+    $content.= '</div>';
+    $content.= '</div>';
+
+    $content.= '</div>';
+    $content.= '</div>';
+}else{
+    $content .= '<div  class="bg-light container py-1">';
 $content .= '<h1 class="bg-light"> ¯\_(ツ)_/¯ Pas encore disponible malheureusement !</h1><br>';
 $content .= '<p class="bg-light">Nous sommes désolé, mais aucun cours n\'est encore ouvert actuellement. Vous pouvez nous contacter ici : <a href="contact.php">page contact</a> pour de plus amples informations à ce sujet.</p>';
 $content .= '<p class="bg-light">Ces cours vous seront proposés dés que possible, et ils permettront à tout un chacun, débutant comme confirmé, de pratiquer le triangle en toute sérénité et bienveillance.</p>';
 $content .= '</div>';
+}
 
 // Affichage title différent page par page avec $title, appelé avant le require du header pour fonctionner.
 $title = "Cours ";
